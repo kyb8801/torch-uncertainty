afe81e0660093de4328a4ddd3f927a98607e3da7
Olivier Laurent <olivier.ar.laurent@gmail.com>
2026-05-10 18:04:27 +0200

    :ok_hand:(packed functional): take @alafage comments into account
    Co-authored-by: Adrien Lafage <adrienlafage@outlook.com>

--- src/torch_uncertainty/layers/functional/packed.py
+++ src/torch_uncertainty/layers/functional/packed.py
@@ -278,10 +278,7 @@ def packed_multi_head_attention_forward(  # noqa: D417
         Implementation of the packed multi-head attention is based on the PyTorch implementation of the
         `torch.nn.MultiheadAttention` module. The implementation is adapted to support packed inputs.
     """
-    if hasattr(F, "_mha_shape_check"):
-        is_batched = F._mha_shape_check(query, key, value, key_padding_mask, attn_mask, num_heads)
-    else:  # coverage: ignore
-        is_batched = query.dim() == 3
+    is_batched = F._mha_shape_check(query, key, value, key_padding_mask, attn_mask, num_heads)  # pyright: ignore[reportAttributeAccessIssue]
 
     # For unbatched input, we unsqueeze at the expected batch-dim to pretend that the input
     # is batched, run the computation and before returning squeeze the
@@ -339,9 +336,9 @@ def packed_multi_head_attention_forward(  # noqa: D417
     )
     if isinstance(embed_dim, Tensor):
         # embed_dim can be a tensor when JIT tracing
-        head_dim = int(embed_dim.div(num_heads, rounding_mode="trunc").item())
+        head_dim = embed_dim.div(num_heads, rounding_mode="trunc")
     else:
-        head_dim = embed_dim // num_heads
+        head_dim = torch.tensor(embed_dim // num_heads)
     assert head_dim * num_heads == embed_dim, (
         f"embed_dim {embed_dim} not divisible by num_heads {num_heads}"
     )
