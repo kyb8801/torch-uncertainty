23eead65ce355b0369b0ed2517ca9320fcb66646
alafage <adrienlafage@outlook.com>
2026-05-12 00:37:42 +0200

    :sparkles: (metrics): implement patched calibration error metrics

--- src/torch_uncertainty/metrics/segmentation/__init__.py
+++ src/torch_uncertainty/metrics/segmentation/__init__.py
@@ -1,5 +1,9 @@
 # ruff: noqa: F401
 from .mean_iou import MeanIntersectionOverUnion
+from .patched_calibration_error import (
+    PatchedBinaryCalibrationError,
+    PatchedMulticlassCalibrationError,
+)
 from .seg_binary_auroc import SegmentationBinaryAUROC
 from .seg_binary_average_precision import SegmentationBinaryAveragePrecision
 from .seg_fpr95 import SegmentationFPR95
