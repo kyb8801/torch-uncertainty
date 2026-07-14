b3eb8847473177609ec81ce9fa7611183af71eab
kyb8801 <150807068+kyb8801@users.noreply.github.com>
2026-07-14 12:50:19 +0900

    Update __init__.py

--- src/torch_uncertainty/post_processing/conformal/__init__.py
+++ src/torch_uncertainty/post_processing/conformal/__init__.py
@@ -1,5 +1,6 @@
 # ruff: noqa: F401
 from .abstract import Conformal
 from .aps import ConformalClsAPS
+from .conformal_reg_cqr import ConformalRegCQR
 from .raps import ConformalClsRAPS
 from .thr import ConformalClsTHR
